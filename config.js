cfbad286e7fdab0ec1558f711b03fe8d0dea88d3
Worstik11 <sadistik22@gmail.com>
2025-08-13 05:04:13 +0300

    Update config.js22

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ export default {
     "host": "lava-v4.ajieblogs.eu.org",
     "port": 443,
     "password": "https://dsc.gg/ajidevserver",
-    "secure": true
+    "secure": true,
       },
     ],
     defaultSearchPlatform: "ytmsearch",
@@ -44,4 +44,5 @@ export default {
   
   activityRotationInterval: 30000, 
 };
+
 
